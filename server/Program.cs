7ecc961bcc23109be2d6b1f1a156d324ba41f7fa
Basit Nawaz <basit.nawaz@wearenova.co.uk>
2024-01-05 23:51:10 +0500

    Changed Project Structure

--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,3 @@
-using IMC_Integrated_Medical_Care_.DbContext;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,23 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DbContextClass>(option =>
-{
-    option.UseSqlServer(builder.Configuration.GetConnectionString("IMCConnectionString"));
-});
-
-
-
-
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-
-    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("IMC")
-    .AddEntityFrameworkStores<DbContextClass>()
-    .AddDefaultTokenProviders();
-
-
-
-
 
 var app = builder.Build();
 

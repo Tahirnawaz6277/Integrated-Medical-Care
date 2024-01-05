c1d5a7b48662024f6c4c810f2677fe737b823721
Basit Nawaz <basit.nawaz@wearenova.co.uk>
2024-01-06 00:37:46 +0500

    fresh install

--- Server/Imc/Controllers/WeatherForecastController.cs
+++ Server/Imc/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace Server.Controllers
+namespace Imc.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Server/Imc/WeatherForecast.cs
+++ Server/Imc/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Server
+namespace Imc
 {
     public class WeatherForecast
     {

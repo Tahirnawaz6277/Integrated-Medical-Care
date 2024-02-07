d95b261b47ce40eb001edfc910a3f77537f01ceb
inamullah00 <inamullahwebpro00@gmail.com>
2024-02-08 01:53:04 +0500

    resolved conflicts

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -12,7 +12,6 @@ import HcpScreen from "./screens/dashboard/HcpScreen";
 import FeedbackScreen from "./screens/dashboard/FeedbackScreen";
 import ServiceScreen from "./screens/dashboard/ServiceScreen";
 import PromotionScreen from "./screens/dashboard/PromotionScreen";
-import { RouteNames } from "./Routes";
 
 function App() {
   return (
@@ -33,12 +32,9 @@ function App() {
         </Route>
 
         {/* Auth Routes */}
-        <Route path={RouteNames.default.route} element={<AuthLayout />}>
+        <Route path="/" element={<AuthLayout />}>
           <Route index element={<LoginScreen />} />
-          <Route
-            path={RouteNames.Auth.Register.route}
-            element={<SignupScreen />}
-          />
+          <Route path="signup" element={<SignupScreen />} />
           <Route path="forgot" element={<ForgotScreen />} />
         </Route>
       </Routes>

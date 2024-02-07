4a8c1128abd376d62e149d9833f6ee83f1ef611b
inamullah00 <inamullahwebpro00@gmail.com>
2024-02-07 22:54:24 +0500

    Login error Isuue done

--- App/src/config/endPoints.js
+++ App/src/config/endPoints.js
@@ -1,8 +1,8 @@
 let BASE_URL = import.meta.env.VITE_API_URL;
-let loginurl = "https://localhost:7270/api";
+
 export const endPoints = {
   Account: {
-    Login: `${loginurl}/Auth/Login`,
+    Login: `${BASE_URL}/Auth/Login`,
     Signup: `${BASE_URL}/Auth/SignUp`,
     GetUsers: `${BASE_URL}/ManageAcount/GetUsers`,
     DeleteUser: `${BASE_URL}/ManageAcount/DeleteUser`,

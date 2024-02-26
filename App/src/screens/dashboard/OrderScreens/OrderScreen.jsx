a3dd3ef34885980cc2a7cc08ad1a612c3881e1d6
Tahirnawaz <tahirnawazwebpro@gmail.com>
2024-02-26 16:32:30 +0100

    Delete functionality added in Order

--- App/src/screens/dashboard/OrderScreens/OrderScreen.jsx
+++ App/src/screens/dashboard/OrderScreens/OrderScreen.jsx
@@ -1,23 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { Button, Card, NavLink, Spinner, Table } from "react-bootstrap";
-import { getOrders } from "../../../services/orderService";
+import { getOrders, DeleteOrder } from "../../../services/orderService";
 
 const OrderScreen = () => {
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrder] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  const updatedOrders = () => {
     getOrders()
       .then((res) => {
         if (res.success) {
-          setOrders(res.data);
-
+          setOrder(res.data);
           setLoading(false);
         }
       })
-      .catch((error) => {
+      .catch((err) => {
         setLoading(true);
       });
+  };
+  const handleDelete = (id) => {
+    DeleteOrder(id)
+      .then((res) => {
+        if (res.success) {
+          updatedOrders();
+        }
+      })
+      .catch((err) => {});
+  };
+  useEffect(() => {
+    updatedOrders();
   }, []);
 
   return (
@@ -53,7 +64,14 @@ const OrderScreen = () => {
                   <td> {order.amount} </td>
                   <td> {order.orderStatus} </td>
                   <td style={{ display: "flex", gap: "8px" }}>
-                    <Button className="btn btn-danger">Delete</Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => {
+                        handleDelete(order.id);
+                      }}
+                    >
+                      Delete
+                    </Button>
                   </td>
                 </tr>
               ))}
